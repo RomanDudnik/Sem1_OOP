fe1a1504699c3dc2f74fe84208c1d563e83f2bc6
«Roman <research777@mail.ru>
2023-03-31 12:08:45 +0300

    TeamsDiv

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,11 +8,11 @@ public class Main {
 //        BaseHero hero = new BaseHero();     // конструктор для героя
         int teamMemberNum = 10;
         System.out.println("\nКоманда 1:");
-        ArrayList<BaseHero> team1 = teams(teamMemberNum);
+        ArrayList<BaseHero> team1 = team1(teamMemberNum);
         team1.forEach(n -> System.out.println(n.name + " " + n.getInfo()));
 
         System.out.println("\nКоманда 2:");
-        ArrayList<BaseHero> team2 = teams(teamMemberNum);
+        ArrayList<BaseHero> team2 = team2(teamMemberNum);
         team2.forEach(n -> System.out.println(n.name + " " + n.getInfo()));
 
         // Созаем объект spell и вызываем его рандомным числом из enum SpellBook:
@@ -25,26 +25,36 @@ public class Main {
        return Names.values()[new Random().nextInt(Names.values().length)].toString();   // рандомно присваиваем имена объектам из массива имен в enum
     }
     // метод для рандомного подбора команды
-    private static ArrayList<BaseHero> teams(int num) {
+    private static ArrayList<BaseHero> team1(int num) {
         ArrayList<BaseHero> heroes = new ArrayList<>();
         for (int i = 0; i < num; i++) {
-            switch (new Random().nextInt(7)) {
+            switch (new Random().nextInt(4)) {
+                case 0:
+                    heroes.add(new Crossbowman(getName()));
+                    break;
+                case 1:
+                    heroes.add(new Monk(getName()));
+                    break;
+                case 2:
+                    heroes.add(new Spearman(getName()));
+                    break;
+                default:
+                    heroes.add(new Peasant(getName()));
+            }
+        }
+        return heroes;
+    }
+    private static ArrayList<BaseHero> team2(int num) {
+        ArrayList<BaseHero> heroes = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            switch (new Random().nextInt(4)) {
                 case 0:
                     heroes.add(new Sniper(getName()));
                     break;
                 case 1:
-                    heroes.add(new Crossbowman(getName()));
-                    break;
-                case 2:
                     heroes.add(new Wizard(getName()));
                     break;
                 case 3:
-                    heroes.add(new Monk(getName()));
-                    break;
-                case 4:
-                    heroes.add(new Spearman(getName()));
-                    break;
-                case 5:
                     heroes.add(new Thief(getName()));
                     break;
                 default:
